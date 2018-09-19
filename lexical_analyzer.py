343e8ab5b74d8932a20223f1a53718e93f9cf1ad
Fredy Álvarez <fre.alvarezb@gmail.com>
2018-09-19 15:35:48 -0500

    Lexical analyzer done

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,5 +1,7 @@
 import reserved_definitions as defs
 import re
+import sys
+import os
 
 
 class Token:
@@ -21,6 +23,8 @@ patter_number = re.compile(
     r'(?:0x[0-9a-f]+|0o[0-7]+|0b[01]+)|(?:\d*\.)?\d+', re.I)
 pattern_valid_name = re.compile(r'[a-z_]\w*', re.I)
 
+pattern_split = re.compile(r'(\W+)')
+
 
 def tokenize(str_list, lineN):
     '''
@@ -30,8 +34,12 @@ def tokenize(str_list, lineN):
     tokens = []
     inString = False
     strTmp = []
-    for item in str_list:
+    for idx, item in enumerate(str_list):
         if inString:
+            if len(item) != 1 and not item.isalnum():
+                for charIdx, char in enumerate(item):
+                    str_list.insert(idx + 1 + charIdx, char)
+                continue
             if item == '"':
                 inString = False
                 tokens.append(
@@ -51,16 +59,44 @@ def tokenize(str_list, lineN):
             tokens.append(Token(token_type, item, lineN))
         elif item == '"':
             inString = True
+            strTmp = []
         elif patter_number.fullmatch(item):
             tokens.append(Token(defs.Token_type.NUMBER, item, lineN))
         elif pattern_valid_name.fullmatch(item):
             tokens.append(Token(defs.Token_type.NAME, item, lineN))
+        elif len(item) != 1:  # Special case: '")', '("', '= "', ...
+            for charIdx, char in enumerate(item):
+                str_list.insert(idx + 1 + charIdx, char)
+        else:
+            tokens.append(Token(defs.Token_type.ERROR,
+                                f"Invalid character {item}", lineN))
 
     if inString:
         tokens.append(Token(defs.Token_type.ERROR,
                             "Wrong number of quotes", lineN))
     return tokens
 
+
+def main():
+    filepath = sys.argv[1]
+
+    if not os.path.isfile(filepath):
+        print(f"File path {filepath} does not exist. Exiting...")
+        sys.exit()
+
+    tokens = []
+    with open(filepath) as file:
+        for lineN, lineVal in enumerate(file):
+            res = tokenize(pattern_split.split(lineVal.strip()), lineN)
+            print(pattern_split.split(lineVal))
+            tokens.extend(res)
+
+    for i in tokens:
+        print(i)
+
+
+if __name__ == '__main__':
+    main()
 
 
 # ver si los strings van a aceptar caracteres de escape:
--- reserved_definitions.py
+++ reserved_definitions.py
@@ -19,119 +19,103 @@ token_types = [
     'NUMBER',
     'STRING',
     'ARRAY_VALS',  # NOTE: Is this needed?
-    'NAME', # represents a variable and function name
+    'NAME',  # represents a variable and function name
 
-    'OPEN_BRACKET', 
+    'OPEN_BRACKET',
     'CLOSE_BRACKET',
     'ERROR',
-    'COMMENT', # Tokens of this type are removed in the lexical analyzer
+    'COMMENT',  # Tokens of this type are removed in the lexical analyzer
 ]
 Token_type = Enum('Token_type', token_types)
 
-types = {
+types = [
     'string',
     'int',
     'float',
     'void',
-}
-
-bool_operators_words = {
+]
+bool_operators_words = [
     'and',
     'or',
     'not'
-}
-
-control_words = {
+]
+control_words = [
     'while',
     'if',
     'else',
     'elif',
     'func',
     'return',
-}
-
-assign_operators = {
+]
+assign_operators = [
     '='
-}
-
-function_operators = {
+]
+function_operators = [
     ':'
-}
-
-arithm_operators = {
+]
+arithm_operators = [
     '+',
     '-',
     '*',
     '/',
     '^',  # right associative
-}
-
-composite_operators = {
+]
+composite_operators = [
     '++',
     '--',
     '*=',
     '+=',
     '-=',
-}
-
-comp_operators = {
+]
+comp_operators = [
     '==',
     '!=',
     '>',
     '<',
     '>=',
     '=>'
-}
-
-bool_operators = {
+]
+bool_operators = [
     '||',
     '&&',
     '!'
-}
-
-open_brackets = {
+]
+open_brackets = [
     '(',
     '[',
     '{'
-}
-
-close_brackets = {
+]
+close_brackets = [
     ')',
     ']',
     '}'
-}
-
-comma_symb = {
+]
+comma_symb = [
     ','
-}
-
-comment_symb = {
+]
+comment_symb = [
     '//'
-}
+]
+
+all_reserved = {}
+all_reserved.update((i, Token_type.TYPE) for i in types)
+all_reserved.update((i, Token_type.ASSIGN_OPERATOR) for i in assign_operators)
+all_reserved.update((i, Token_type.ARITHMETIC_OPERATOR) for i in arithm_operators)
+all_reserved.update((i, Token_type.COMPARATION_OPERATOR) for i in comp_operators)
+all_reserved.update((i, Token_type.BOOLEAN_OPERATOR) for i in bool_operators)
+all_reserved.update((i, Token_type.BOOLEAN_OPERATOR) for i in bool_operators_words)
+all_reserved.update((i, Token_type.FUNCTION_OPERATOR) for i in function_operators)
+all_reserved.update((i, Token_type.CONTROL_WORD) for i in control_words)
+all_reserved.update((i, Token_type.COMPOSITE_OPERATOR) for i in composite_operators)
+all_reserved.update((i, Token_type.OPEN_BRACKET) for i in open_brackets)
+all_reserved.update((i, Token_type.CLOSE_BRACKET) for i in close_brackets)
+all_reserved.update((i, Token_type.COMMA) for i in comma_symb)
+all_reserved.update((i, Token_type.COMMENT) for i in comment_symb)
 
 def check_reserved(item):
-    if item in types:
-        return Token_type.TYPE
-    if item in assign_operators:
-        return Token_type.ASSIGN_OPERATOR
-    if item in arithm_operators:
-        return Token_type.ARITHMETIC_OPERATOR
-    if item in comp_operators:
-        return Token_type.COMPARATION_OPERATOR
-    if item in bool_operators or item in bool_operators_words:
-        return Token_type.BOOLEAN_OPERATOR
-    if item in function_operators:
-        return Token_type.FUNCTION_OPERATOR
-    if item in control_words:
-        return Token_type.CONTROL_WORD
-    if item in composite_operators:
-        return Token_type.COMPOSITE_OPERATOR
-    if item in open_brackets:
-        return Token_type.OPEN_BRACKET
-    if item in close_brackets:
-        return Token_type.CLOSE_BRACKET
-    if item in comma_symb:
-        return Token_type.COMMA
-    if item in comment_symb:
-        return Token_type.COMMENT
-    return None
+    if item in all_reserved:
+        return all_reserved[item]
+    else:
+        return None
+
+
