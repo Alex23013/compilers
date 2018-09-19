076ac7a94293e0225a7f8fd00b9ffa0bbdbf1a7f
Fredy Álvarez <fre.alvarezb@gmail.com>
2018-09-19 00:10:52 -0500

    Improved and simplier lexical analyzer

--- reserved_definitions.py
+++ reserved_definitions.py
@@ -6,28 +6,30 @@ token_types = [
     'ARITHMETIC_OPERATOR',
     'COMPARATION_OPERATOR',
     'BOOLEAN_OPERATOR',
-    'FUNCTION_OPERATOR'
+    'COMPOSITE_OPERATOR',
+    'FUNCTION_OPERATOR',
+    'COMMA',
 
     # Reserved words
     'TYPE',
-    'CONTROL_STRUCT',
-    'FUNCTION_DECLARATION',
-    'FUNCTION_CALL',
-    'RESERVED_WORD',  # Other reserver words
+    'CONTROL_WORD',
+    'RESERVED_WORD',  # Other reserver words # NOTE: Is this needed?
 
     # Variables and values
-    'VARIABLE',
     'NUMBER',
     'STRING',
     'ARRAY_VALS',  # NOTE: Is this needed?
+    'NAME', # represents a variable and function name
 
-    'BRACKET',  # Any kind of bracket
+    'OPEN_BRACKET', 
+    'CLOSE_BRACKET',
     'ERROR',
+    'COMMENT', # Tokens of this type are removed in the lexical analyzer
 ]
 Token_type = Enum('Token_type', token_types)
 
 types = {
-    'char',
+    'string',
     'int',
     'float',
     'void',
@@ -99,21 +101,37 @@ close_brackets = {
     '}'
 }
 
-comma_operators = {
+comma_symb = {
     ','
 }
 
-reserved_words = [
-    types,
-    bool_operators_words,
-    control_words,
-    assign_operators,
-    function_operators,
-    arithm_operators,
-    composite_operators,
-    comp_operators,
-    bool_operators,
-    open_brackets,
-    close_brackets,
-    comma_operators,
-]
+comment_symb = {
+    '//'
+}
+
+def check_reserved(item):
+    if item in types:
+        return Token_type.TYPE
+    if item in assign_operators:
+        return Token_type.ASSIGN_OPERATOR
+    if item in arithm_operators:
+        return Token_type.ARITHMETIC_OPERATOR
+    if item in comp_operators:
+        return Token_type.COMPARATION_OPERATOR
+    if item in bool_operators or item in bool_operators_words:
+        return Token_type.BOOLEAN_OPERATOR
+    if item in function_operators:
+        return Token_type.FUNCTION_OPERATOR
+    if item in control_words:
+        return Token_type.CONTROL_WORD
+    if item in composite_operators:
+        return Token_type.COMPOSITE_OPERATOR
+    if item in open_brackets:
+        return Token_type.OPEN_BRACKET
+    if item in close_brackets:
+        return Token_type.CLOSE_BRACKET
+    if item in comma_symb:
+        return Token_type.COMMA
+    if item in comment_symb:
+        return Token_type.COMMENT
+    return None
